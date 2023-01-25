f498c2b0703f56c057f037ee8c434ad4ce0972bc
manuel.tecnico <manuel.tecnico@activaelkoko.com>
2023-01-25 16:05:47 +0100

    Feat: token semi implementado

--- src/main/java/es/eukariotas/apiservice/exceptions/CustomExceptions.java
+++ src/main/java/es/eukariotas/apiservice/exceptions/CustomExceptions.java
@@ -1,4 +1,20 @@
 package es.eukariotas.apiservice.exceptions;
 
 public class CustomExceptions extends Exception{
+    public CustomExceptions(String message) {
+        super(message);
+    }
+
+    public CustomExceptions() {
+        super("Error en la peticion");
+    }
+
+    public CustomExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomExceptions(Throwable cause) {
+        super(cause);
+    }
+
 }
--- src/main/java/es/eukariotas/apiservice/service/GenericService.java
+++ src/main/java/es/eukariotas/apiservice/service/GenericService.java
@@ -45,4 +45,5 @@ public class GenericService {
         }
         return headers;
     }
+
 }
